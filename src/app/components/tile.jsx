ee03af2bae02e162ddfbc1425c3f48fe8fb36971
jchang6513 <31400914+jchang6513@users.noreply.github.com>
2018-06-01 21:40:45 +0800

    update

--- src/app/components/tile.jsx
+++ src/app/components/tile.jsx
@@ -8,106 +8,76 @@ Number.prototype.pad = function(size) {
   return s;
 }
 
-function getURL(date) {
-    var start = new Date(date._d.getFullYear(), 0, 0);
-    var diff = (date._d-start)+((start.getTimezoneOffset()-date._d.getTimezoneOffset())*60000);
-    var doy = Math.floor(diff/86400000);
-
-    var url='http://irsl.ss.ncu.edu.tw/media/product/TWRG/';
-    url += date._d.getFullYear().toString()+'.'+doy.pad(3)+'/'
-    url += 'TWRG'+doy.pad(3)+String.fromCharCode(65+date._d.getHours())+'.'+date._d.getFullYear().toString().substr(2)+'I.'+date._d.getMinutes().pad(2)+'.png';
-    return url;
-}
-
 export class Tile extends React.Component {
     constructor () {
         super();
         this.state = {
             beginDate: moment("2017010100", "YYYYMMDDHH"),
-            currentDate: moment().subtract(5+moment().utcOffset()/60,'hours').startOf('hour'),
-            mainSrc: moment().subtract(5+moment().utcOffset()/60,'hours').startOf('hour'),
-            nextSrc: moment().subtract(4+moment().utcOffset()/60,'hours').startOf('hour'),
-            prevSrc: moment().subtract(6+moment().utcOffset()/60,'hours').startOf('hour'),
+            currentDate: moment().subtract(moment().utcOffset()/60,'hours').startOf('hour'),
+            mainSrc: moment().subtract(moment().utcOffset()/60,'hours').startOf('hour'),
+            nextSrc: moment().subtract(moment().utcOffset()/60,'hours').startOf('hour'),
+            prevSrc: moment().subtract(moment().utcOffset()/60,'hours').startOf('hour'),
             currentHour: 0,
             isOpen: false,
         };
     }
 
     componentDidMount () {
-
+        this.setSrc(0)
+        this.setState({
+            beginDate: this.props.beginDate,
+            currentDate: moment().subtract(this.props.delay+moment().utcOffset()/60,'hours').startOf('hour'),
+        });
     }
-    
-    handleClickImage = () => {
-  		if (this.state.currentImage === this.state.urls.length - 1) return;
-  		this.gotoNext();
+
+    toggleLightbox = () => {
+        this.setSrc(0)
+    		this.setState({
+            isOpen: !this.state.isOpen,
+    		});
   	}
 
-  	toggleLightbox = () => {
-  		this.setState({
-          mainSrc: moment().subtract(5+moment().utcOffset()/60,'hours').startOf('hour'),
-          nextSrc: moment().subtract(4+moment().utcOffset()/60,'hours').startOf('hour'),
-          prevSrc: moment().subtract(6+moment().utcOffset()/60,'hours').startOf('hour'),
-          isOpen: !this.state.isOpen,
-  		});
+    handleClickImage = () => {
+    		if (this.state.currentImage === this.state.urls.length - 1) return;
+    		this.gotoNext();
   	}
 
   	gotoPrevious = () => {
         const currentHour = (this.state.prevSrc.isBefore(this.state.beginDate)) ? 0 : this.state.currentHour+1
-    		this.setState({
-            mainSrc: moment().subtract(5+currentHour+moment().utcOffset()/60,'hours').startOf('hour'),
-            nextSrc: moment().subtract(4+currentHour+moment().utcOffset()/60,'hours').startOf('hour'),
-            prevSrc: moment().subtract(6+currentHour+moment().utcOffset()/60,'hours').startOf('hour'),
-            currentHour: currentHour
-    		});
+    		this.setSrc(currentHour)
   	}
+
   	gotoNext = () => {
         const currentHour = (this.state.nextSrc.isAfter(this.state.currentDate)) ? this.state.currentDate.diff(this.state.beginDate,'hours') : this.state.currentHour-1
-        this.setState({
-          mainSrc: moment().subtract(5+currentHour+moment().utcOffset()/60,'hours').startOf('hour'),
-          nextSrc: moment().subtract(4+currentHour+moment().utcOffset()/60,'hours').startOf('hour'),
-          prevSrc: moment().subtract(6+currentHour+moment().utcOffset()/60,'hours').startOf('hour'),
-          currentHour: currentHour
-        });
+    		this.setSrc(currentHour)
   	}
 
-    getImg = () => {
-        var Images = [];
-        var date = moment(this.state.startDate);
-        while(this.state.endDate.isAfter(date)) {
-            Images.push(this.getURL(date))
-            date.add(1,'hour');
-        }
+    setSrc = (currentHour) => {
         this.setState({
-            urls: Images,
-            iurl: 0
-        })
+            mainSrc: moment().subtract(this.props.delay+currentHour+moment().utcOffset()/60,'hours').startOf('hour'),
+            nextSrc: moment().subtract(this.props.delay-1+currentHour+moment().utcOffset()/60,'hours').startOf('hour'),
+            prevSrc: moment().subtract(this.props.delay+1+currentHour+moment().utcOffset()/60,'hours').startOf('hour'),
+            currentHour: currentHour
+        });
     }
-
     render() {
         return (
-          <div id="TGIM" className='row'>
-              <div className="header jumbotron">
-                  <h1 id="title">TGIM</h1>
-              </div>
-              <div className='col-9 mt-4 float-right mx-auto'>
-                  <a onClick={this.toggleLightbox}>
-                      <img className="img-fluid" src={getURL(this.state.currentDate)} alt=""></img>
-                  </a>
-              </div>
-
+          <div className="col-lg-2 col-md-4 col-6 p-0 m-0">
+              <a href='#' onClick={this.toggleLightbox}>
+                  <img className="d-block" src={this.props.img}/>
+              </a>
               {this.state.isOpen && (
                     <Lightbox
-                        mainSrc={getURL(this.state.mainSrc)}
-                        nextSrc={getURL(this.state.nextSrc)}
-                        prevSrc={getURL(this.state.nextSrc)}
+                        mainSrc={this.props.getURL(this.state.mainSrc)}
+                        nextSrc={this.props.getURL(this.state.nextSrc)}
+                        prevSrc={this.props.getURL(this.state.nextSrc)}
                         onCloseRequest={() => this.setState({ isOpen: false })}
                         onMovePrevRequest={this.gotoPrevious}
                         onMoveNextRequest={this.gotoNext}
-                        imagePadding={100}
-                        imageCaption={<p>For more information, please visit <a href="http://www.ss.ncu.edu.tw/~istep/tgim.html">http://www.ss.ncu.edu.tw/~istep/tgim.html</a></p>}
+                        imagePadding={this.props.imagePadding}
+                        imageCaption={this.props.imageCaption}
                     />
               )}
-
           </div>
         );
     }
--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,40 +1,91 @@
 import React from 'react';
 import { render } from 'react-dom';
-//import { Tile } from './components/til.jsx';
+
+import moment from 'moment';
+import { Tile } from './components/tile.jsx';
 
 class App extends React.Component {
+    constructor () {
+        super();
+        this.state = {
+          Slide1: 'http://localhost:8080/images/Slide1.PNG',
+          Slide2: 'http://localhost:8080/images/Slide2.PNG',
+          Slide3: 'http://localhost:8080/images/Slide3.PNG',
+          Slide4: 'http://localhost:8080/images/Slide4.PNG',
+          Slide5: 'http://localhost:8080/images/Slide5.PNG',
+          Slide6: 'http://localhost:8080/images/Slide6.PNG',
+        };
+    }
+
+    getTWRG = (date) => {
+        var start = new Date(date._d.getFullYear(), 0, 0);
+        var diff = (date._d-start)+((start.getTimezoneOffset()-date._d.getTimezoneOffset())*60000);
+        var doy = Math.floor(diff/86400000);
+
+        var url='http://irsl.ss.ncu.edu.tw/media/product/TWRG/';
+        url += date._d.getFullYear().toString()+'.'+doy.pad(3)+'/'
+        url += 'TWRG'+doy.pad(3)+String.fromCharCode(65+date._d.getHours())+'.'+date._d.getFullYear().toString().substr(2)+'I.'+date._d.getMinutes().pad(2)+'.png';
+        return url;
+    }
+
+    getTWRR = (date) => {
+        var start = new Date(date._d.getFullYear(), 0, 0);
+        var diff = (date._d-start)+((start.getTimezoneOffset()-date._d.getTimezoneOffset())*60000);
+        var doy = Math.floor(diff/86400000);
+
+        var url='http://irsl.ss.ncu.edu.tw/media/product/TWRR/';
+        url += date._d.getFullYear().toString()+'.'+doy.pad(3)+'/'
+        url += 'TWRR'+doy.pad(3)+String.fromCharCode(65+date._d.getHours())+'.'+date._d.getFullYear().toString().substr(2)+'I.'+date._d.getMinutes().pad(2)+'.png';
+        return url;
+    }
+
+    getDOP = (date) => {
+        var start = new Date(date._d.getFullYear(), 0, 0);
+
+        var url='http://irsl.ss.ncu.edu.tw/media/product/Doppler/';
+        url += date.format('YYYY/MM/')
+        url += 'DOP'+date.format('YYYYMMDD.HH')+'.png';
+        return url;
+    }
+
     render () {
         return (
             <div className="container-fluid">
                 <div className="row">
                     <div className="col-lg-2 col-md-4 col-6 p-0 m-0">
                         <a href="pre.html">
-                            <img className="d-block" src="images/Slide1.PNG" style="width:100%;"/>
+                            <img className="d-block" src={this.state.Slide1}/>
                         </a>
                     </div>
-                    <div className="col-lg-2 col-md-4 col-6 p-0 m-0">
-                        <a href="tgim.html">
-                            <img className="d-block" src="images/Slide7.PNG" style="width:100%;"/>
-                        </a>
-                    </div>
-                    <div className="col-lg-2 col-md-4 col-6 p-0 m-0">
-                        <a href="trim.html">
-                            <img className="d-block" src="images/Slide4.PNG" style="width:100%;"/>
-                        </a>
-                    </div>
-                    <div className="col-lg-2 col-md-4 col-6 p-0 m-0">
-                        <a href="dop.html">
-                            <img className="d-block" src="images/Slide3.PNG" style="width:100%;"/>
-                        </a>
-                    </div>
+                    <Tile
+                        img={this.state.Slide2}
+                        beginDate={moment("2018010100", "YYYYMMDDHH")}
+                        delay={5}
+                        getURL={this.getTWRG}
+                        imageCaption={<p>For more information, please visit <a href='http://www.ss.ncu.edu.tw/~istep/tgim.html'>http://www.ss.ncu.edu.tw/~istep/tgim.html</a></p>}
+                    />
+                    <Tile
+                        img={this.state.Slide3}
+                        beginDate={moment("2018010100", "YYYYMMDDHH")}
+                        delay={3}
+                        getURL={this.getTWRR}
+                        imageCaption={<p>For more information, please visit <a href='http://www.ss.ncu.edu.tw/~istep/trim.html'>http://www.ss.ncu.edu.tw/~istep/trim.html</a></p>}
+                    />
+                    <Tile
+                        img={this.state.Slide4}
+                        beginDate={moment("2017010100", "YYYYMMDDHH")}
+                        delay={-5}
+                        getURL={this.getDOP}
+                        imageCaption={<p>For more information, please visit <a href='http://www.ss.ncu.edu.tw/~istep/dop.html'>http://www.ss.ncu.edu.tw/~istep/dop.html</a></p>}
+                    />
                     <div className="col-lg-2 col-md-4 col-6 p-0 m-0">
                         <a href="qf.html">
-                            <img className="d-block" src="images/Slide5.PNG" style="width:100%;"/>
+                            <img className="d-block" src={this.state.Slide5}/>
                         </a>
                     </div>
                     <div className="col-lg-2 col-md-4 col-6 p-0 m-0">
                         <a href="inf.html">
-                            <img className="d-block" src="images/Slide6.PNG" style="width:100%;"/>
+                            <img className="d-block" src={this.state.Slide6}/>
                         </a>
                     </div>
                 </div>
